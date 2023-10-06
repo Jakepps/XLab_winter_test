454a5dc395497e8d94f4c2dab65713b503f9e58c
Artem N. <a.nagalevskiy@xlab-game.com>
2023-10-06 18:00:02 +0300

    камни падают

--- test_xlab/Assets/Scripts/PlayerController.cs
+++ test_xlab/Assets/Scripts/PlayerController.cs
@@ -2,34 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TestXlab
+namespace Golf
 {
-
     public class PlayerController : MonoBehaviour
     {
-		public Spawner spawner;
-		public CloudController cloudController;
-		public WeaponController weaponController;
-
-		private void Update()
-		{
-			if (Input.GetKeyDown(KeyCode.X))
-			{
-				Debug.Log("X key down");
-				spawner.Spawn();
-			}
-
-			if (Input.GetKeyDown(KeyCode.Z))
-			{
-				Debug.Log("Z key down");
-				cloudController.Action();
-			}
-
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				Debug.Log("Space key down");
-				weaponController.Change();
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0)) {
+                Debug.Log("Mouse down!");
             }
-		}
-	}
+        }
+    }
 }
