7664bd6ab3a8124c34f52c5e8db9b3a9167aa2b2
Jakepps <nagalevsiy@mail.ru>
2023-10-08 17:21:21 +0300

    Настроил дождь

--- test_xlab/Assets/Scripts/CloudController.cs
+++ test_xlab/Assets/Scripts/CloudController.cs
@@ -8,13 +8,13 @@ namespace TestXlab
 	public class CloudController : MonoBehaviour
 	{
 		public Transform[] targets;
-		public Transform cloud;
-        public GameObject rainPrefab;
+		public Cloud cloud;
+        //public GameObject rainPrefab;
 
         private bool m_moved = false;
         public float moveSpeed = 12f;
 
-        private int m_targetIndex = -1;
+        private int m_targetIndex = 0;
         private float initialCloudHeight;
 
         private bool isRain = false;
@@ -29,7 +29,8 @@ namespace TestXlab
             }
 
             m_moved = true;
-            isRain = false;
+            //isRain = false;
+            cloud.StopFx();
 
             m_targetIndex++;
             if (m_targetIndex >= targets.Length)
@@ -40,7 +41,7 @@ namespace TestXlab
 
         private void Start()
         {
-            initialCloudHeight = cloud.position.y;
+            initialCloudHeight = cloud.transform.position.y;
         }
 
         private void Update()
@@ -55,31 +56,27 @@ namespace TestXlab
             
             Vector3 targetPosition = new Vector3(target.position.x, initialCloudHeight, target.position.z);
 
-            Vector3 offset = (targetPosition - cloud.position).normalized * moveSpeed * Time.deltaTime;
+            Vector3 offset = (targetPosition - cloud.transform.position).normalized * moveSpeed * Time.deltaTime;
 
-            if (Vector3.Distance(cloud.position, targetPosition) < offset.magnitude)
+            if (Vector3.Distance(cloud.transform.position, targetPosition) < offset.magnitude)
             {
-                 cloud.position = targetPosition;
-                 m_moved = false;
+                cloud.transform.position = targetPosition;
+                m_moved = false;
 
-                 if (!m_moved)
-                 {
-                    isRain = true;
-                    StartCoroutine(StartRain());
-                 } 
+                cloud.PlayFX();
             }
             else
             {
-                cloud.Translate(offset);
+                cloud.transform.Translate(offset);
             }
         }
-        private IEnumerator StartRain()
-        {
-            while (isRain)
-            {
-                Instantiate(rainPrefab, cloud.position + new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-0.2f, 0.2f), Random.Range(-1.5f, 1.5f)), Quaternion.identity);
-                yield return new WaitForSeconds(0.01f);
-            }
-        }
+        //private IEnumerator StartRain()
+        //{
+        //    while (isRain)
+        //    {
+        //        Instantiate(rainPrefab, cloud.transform.position + new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-0.2f, 0.2f), Random.Range(-1.5f, 1.5f)), Quaternion.identity);
+        //        yield return new WaitForSeconds(0.01f);
+        //    }
+        //}
     }
 }
